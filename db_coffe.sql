315269dd5b93bc2e0306834638fe8fdadd75f478
kevinhaka-01 <154268541+kevinhaka-01@users.noreply.github.com>
2023-12-19 04:21:01 +0800

    sql

---
+++ db_coffe.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 18, 2023 at 09:12 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_coffe`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategoriproduk`
+--
+-- Error reading structure for table db_coffe.kategoriproduk: #1932 - Table &#039;db_coffe.kategoriproduk&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_coffe.kategoriproduk: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_coffe`.`kategoriproduk`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggan`
+--
+-- Error reading structure for table db_coffe.pelanggan: #1932 - Table &#039;db_coffe.pelanggan&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_coffe.pelanggan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_coffe`.`pelanggan`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesanan`
+--
+-- Error reading structure for table db_coffe.pesanan: #1932 - Table &#039;db_coffe.pesanan&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_coffe.pesanan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_coffe`.`pesanan`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+-- Error reading structure for table db_coffe.produk: #1932 - Table &#039;db_coffe.produk&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_coffe.produk: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_coffe`.`produk`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `promo`
+--
+-- Error reading structure for table db_coffe.promo: #1932 - Table &#039;db_coffe.promo&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_coffe.promo: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_coffe`.`promo`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rincianpesanan`
+--
+-- Error reading structure for table db_coffe.rincianpesanan: #1932 - Table &#039;db_coffe.rincianpesanan&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_coffe.rincianpesanan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_coffe`.`rincianpesanan`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
